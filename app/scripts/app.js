96f6739c0744675edffa630ba4fc7aa2534cc232
RoYa <roya3000@ukr.net>
2016-01-09 11:59:39 +0200

    init app

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name wikiSearchApp
+ * @description
+ * # wikiSearchApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('wikiSearchApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name wikiSearchApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the wikiSearchApp
+ */
+angular.module('wikiSearchApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
