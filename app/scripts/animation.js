34f9307febe042a8613102e6766ac7717472a7ab
RoYa <roya3000@ukr.net>
2016-01-09 16:44:59 +0200

    refactor animation and remove classie.js

---
+++ app/scripts/animation.js
@@ -0,0 +1,50 @@
+(function() {
+  'use strict';
+
+  var morphSearch = document.getElementById( 'morphsearch' ),
+    input = morphSearch.querySelector( 'input.morphsearch-input' ),
+    ctrlClose = morphSearch.querySelector( 'span.morphsearch-close' ),
+    isOpen = false,
+  // show/hide search area
+    toggleSearch = function(evt) {
+      // return if open and the input gets focused
+      if( evt.type.toLowerCase() === 'focus' && isOpen ) return false;
+
+      var offsets = morphsearch.getBoundingClientRect();
+      if( isOpen ) {
+        morphSearch.classList.remove('open');
+
+
+        // trick to hide input text once the search overlay closes
+        // todo: hardcoded times, should be done after transition ends
+        if( input.value !== '' ) {
+          setTimeout(function() {
+            morphSearch.classList.add('hideInput');
+            setTimeout(function() {
+              morphSearch.classList.remove('hideInput');
+              input.value = '';
+            }, 300 );
+          }, 500);
+        }
+
+        input.blur();
+      }
+      else {
+        morphSearch.classList.add('open');
+      }
+      isOpen = !isOpen;
+    };
+
+  // events
+  input.addEventListener( 'focus', toggleSearch );
+  ctrlClose.addEventListener( 'click', toggleSearch );
+  // esc key closes search overlay
+  // keyboard navigation events
+  document.addEventListener( 'keydown', function( ev ) {
+    var keyCode = ev.keyCode || ev.which;
+    if( keyCode === 27 && isOpen ) {
+      toggleSearch(ev);
+    }
+  } );
+
+})();
